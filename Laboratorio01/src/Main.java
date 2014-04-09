80fdca53745c63f3110660c3f69c0224d3c2f095
Alumno <Alumno@eq2-lab1>
2014-04-09 19:26:06 -0300

    commin inicial

---
+++ Laboratorio01/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	
+	public static void main(String[] args){
+		
+		Perro perro1 = new Perro();
+		perro1.nombre = "Bobby";
+		perro1.ladrar();
+		
+		Perro perro2 = new Perro();
+		perro2.nombre = "HD";
+		perro2.ladrar();
+		
+	}
+
+}
---
+++ Laboratorio01/src/Perro.java
@@ -0,0 +1,11 @@
+
+public class Perro {
+	
+	String nombre; 
+	
+	void ladrar(){
+		
+		System.out.println(nombre+ " Dijo: Guau guau guau guau guau");
+	}
+
+}
